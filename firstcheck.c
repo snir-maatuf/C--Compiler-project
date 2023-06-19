06c71b5ce84a7011e82cdfd060ba32b75dfeed32
snir-maatuf <122987145+snir-maatuf@users.noreply.github.com>
2023-06-19 11:52:32 +0300

    Update firstcheck.c

--- firstcheck.c
+++ firstcheck.c
@@ -1,1 +1,8 @@
 #include "errors.h"
+
+/* chack the excactly type of instructions (Like: instruction start in point "." and more...) */
+
+/* func that runs on each line the next code: */
+/* isSymbol || enctern&extern || string&data */
+
+/* call to dcode functions from dcode.h */
